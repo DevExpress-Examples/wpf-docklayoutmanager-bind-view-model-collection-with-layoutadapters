d30a1e64f494f6a020abc436712027ceb1d6df63
DevExpressExampleBot <fakeEmail>
2021-01-11 15:55:10 +0300

    autogenerate VB

---
+++ VB/DXSample/Views/MainView.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Windows.Controls
+
+Namespace DXSample.Views
+	''' <summary>
+	''' Interaction logic for View1.xaml
+	''' </summary>
+	Partial Public Class MainView
+		Inherits UserControl
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
